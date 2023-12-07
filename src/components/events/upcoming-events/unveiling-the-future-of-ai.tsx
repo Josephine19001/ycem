855bdc9f2990d9eedf072d75c87f6f3a83cebd2d
josephine19001 <gyameraj@gmail.com>
2023-12-07 23:29:21 +0200

    Add unveiling the future of AI event

---
+++ src/components/events/upcoming-events/unveiling-the-future-of-ai.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import EventPage from "../event-page";
+import banner from "../../../../public/assets/events/unveiling-ai-event.jpg";
+import { FlexContainerColumnGapped } from "../../shared/containers";
+
+const UnveilingTheFutureOfAI = () => {
+  return (
+    <EventPage
+      timeDay={"Saturday, 3:30PM - 5:00PM GMT"}
+      date="December 16th, 2023"
+      location="Online, Zoom"
+      eventLink="https://forms.gle/1qX8W8je7hH67aq67"
+      eventBanner={banner}
+      description={
+        <FlexContainerColumnGapped $alignitems="flex-start">
+          <i>
+            Peek into the future of AI: where algorithms meet imagination,
+            unlocking tomorrow's possibilities!{""}
+          </i>
+          <p>
+            YCEM is thrilled to present:{" "}
+            <b>'Unveiling the Future of Artificial Intelligence'!</b> Brace
+            yourself for an immersive journey into the world of AI at our
+            upcoming webinar event. Join us as we explore key topics, unraveling
+            the fundamentals and showcasing diverse applications in today's
+            dynamic world. Whether you're a tech enthusiast, a budding
+            professional, or simply curious about AI's possibilities, this event
+            is your gateway to understanding the present and shaping the future.
+          </p>
+          <div
+            style={{
+              marginTop: "16px",
+            }}
+          >
+            <h5>Key Topics:</h5>
+            <ul className="event-ul" style={{ marginBottom: "12px" }}>
+              <li>Introduction to Artificial Intelligence</li>
+              <li>Applications of AI in Today's World</li>
+              <li>AI Quizzes and Games</li>
+              <li>Career Paths in AI</li>
+            </ul>
+          </div>
+          <div>
+            <h5>Who Should Attend?</h5>
+            <ul className="event-ul">
+              <li>Senior High School to University Students</li>
+              <li>All levels of AI familiarity welcome!</li>
+            </ul>
+          </div>
+        </FlexContainerColumnGapped>
+      }
+    />
+  );
+};
+
+export default UnveilingTheFutureOfAI;
