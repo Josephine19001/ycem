8aaa283d9cf8fd498d2e919d308e014b15af273f
Agyeiwaa <136027473+Agyeiwaa9@users.noreply.github.com>
2024-10-09 22:09:50 +0000

    Resolving conflicts 2

--- src/components/events/past-events/events.tsx
+++ src/components/events/past-events/events.tsx
@@ -7,12 +7,8 @@ import exploringBioInformaticPic from 'public/assets/events/exploring-bioinforma
 import biomedMedicineFLyer from 'public/assets/events/biomed-medicine-flyer.png';
 import aiMedicineFLyer from 'public/assets/events/ai-in-medicine.png';
 import cloundWebinar from 'public/assets/events/cloud-webinar.png';
-
 import accraGirlsEvent from 'public/assets/events/accragirlsevent-flyer.png';
-
 import ycemDunk from 'public/assets/events/ycem-dunk.png';
-
-
 import { Container } from '../upcoming-events/events';
 import EventItem from '../event-list-item';
 
