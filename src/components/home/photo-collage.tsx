280caf7a61b78b673c5007cf16a40e954d526226
josephine19001 <gyameraj@gmail.com>
2024-10-16 21:47:55 +0300

    Fixed collage

--- src/components/home/photo-collage.tsx
+++ src/components/home/photo-collage.tsx
@@ -1,14 +1,13 @@
 import { Row, Col } from 'react-bootstrap';
 import Image from 'next/image';
-import img1 from 'public/photo-collage/image2.jpg';
-import img2 from 'public/photo-collage/IMG_P4.jpg'; //small and horizontal
-import img4 from 'public/photo-collage/IMG-PC5.jpg';
+import img2 from 'public/photo-collage/IMG_P4.jpg';
+import img4 from 'public/photo-collage/image2.jpg';
 import img5 from 'public/photo-collage/IMG6.jpg';
 import img6 from 'public/photo-collage/image7.jpg';
 import img7 from 'public/photo-collage/IMG8.jpg';
-import img8 from 'public/photo-collage/IMG10.jpg'; //small and horizontal
-import img9 from 'public/photo-collage/IMG_P12.jpg'; //small and horizontal
-import img10 from 'public/photo-collage/IMG12.jpg'; //small and horizontal
+import img8 from 'public/photo-collage/IMG10.jpg';
+import img9 from 'public/photo-collage/IMG_P12.jpg';
+import img10 from 'public/photo-collage/image10.jpg';
 
 const PhotoCollage = () => {
   return (
@@ -16,7 +15,7 @@ const PhotoCollage = () => {
       <Row className="d-flex flex-row flex-wrap justify-content-center">
         <Col xs={12} sm={6} md={3} lg={3} className="d-flex flex-column">
           <Image src={img4} alt="Image 4" />
-          <Image src={img2} alt="Image 2" />
+          <Image src={img8} alt="Image 8" className="m-1 p-1" />
         </Col>
 
         <Col xs={12} sm={6} md={3} lg={3} className="d-flex flex-column">
@@ -25,8 +24,8 @@ const PhotoCollage = () => {
         </Col>
 
         <Col xs={12} sm={6} md={3} lg={3} className="d-flex flex-column">
-          <Image src={img8} alt="Image 8" className="m-1 p-1" />
           <Image src={img6} alt="Image 6" className="scale mb-2" />
+          <Image src={img2} alt="Image 2" />
         </Col>
 
         <Col xs={12} sm={6} md={3} lg={3} className="d-flex flex-column">
